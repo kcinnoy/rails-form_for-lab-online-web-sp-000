7c41ad9bb19056d21487a5b7178d27da70cb2976
Yonnick <yonnick00@gmail.com>
2019-07-06 13:41:02 +0000

    Automatically backed up by Learn

--- spec/controllers/school_classes_controller_spec.rb
+++ spec/controllers/school_classes_controller_spec.rb
@@ -9,32 +9,5 @@ RSpec.describe SchoolClassesController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
